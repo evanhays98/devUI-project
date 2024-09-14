fb979b76d2bc9db58f24016325ec50e8853e6b73
evanhays98 <evan.hays@epita.fr>
2024-09-14 23:12:28 +0200

    add app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './Pages/AppRouter';
-import { StyleProvider } from './libs/core/StyleContext';
-import { CreateComponentProvider } from './libs/core';
 
 function App() {
   return (
     <React.StrictMode>
       <BrowserRouter>
-        <StyleProvider>
-          <CreateComponentProvider>
-            <AppRouter />
-          </CreateComponentProvider>
-        </StyleProvider>
+        <AppRouter />
       </BrowserRouter>
     </React.StrictMode>
   );
