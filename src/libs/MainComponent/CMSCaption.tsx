14f62ec48665a13069b856db83bdb1561efbc086
evanhays98 <evan.hays@epita.fr>
2024-09-14 13:14:57 +0200

    add label

--- src/libs/MainComponent/CMSCaption.tsx
+++ src/libs/MainComponent/CMSCaption.tsx
@@ -1,80 +1,64 @@
-import React, { forwardRef, useMemo } from 'react';
+import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { theme, Theme } from '../theme';
-import { useCustomStyle } from '../../core/StyleContext';
-import { v4 as uuidv4 } from 'uuid';
 
-const useStyles = createUseStyles<string, { style: any }, any>(
-  (theme: Theme) => ({
-    text: (props) => ({
-      fontFamily: 'Montserrat, sans-serif',
-      color: theme.colors.black,
+const useStyles = createUseStyles<string, {}, any>(
+  (theme: Theme) => (@Start{
+  text: {
+    fontFamily: 'Montserrat, sans-serif',
+      color: '@Stheme.colors.black@E',
       fontWeight: 300,
       fontSize: 13,
       lineHeight: 1.5,
       margin: 0,
       width: 'fit-content',
       borderBottom:
-        props.style?.desktop?.borderBottom ||
-        props.style.desktop?.border ||
-        'none',
+    theme?.desktop?.borderBottom ||
+    theme.desktop?.border ||
+    'none',
       borderLeft:
-        props.style?.desktop?.borderLeft ||
-        props.style.desktop?.border ||
-        'none',
+    theme?.desktop?.borderLeft ||
+    theme.desktop?.border ||
+    'none',
       borderRight:
-        props.style?.desktop?.borderRight ||
-        props.style.desktop?.border ||
-        'none',
+    theme?.desktop?.borderRight ||
+    theme.desktop?.border ||
+    'none',
       borderTop:
-        props.style?.desktop?.borderTop ||
-        props.style.desktop?.border ||
-        'none',
-      ...props.style?.desktop,
+    theme?.desktop?.borderTop ||
+    theme.desktop?.border ||
+    'none',
+  ...theme?.desktop,
 
       '&:hover': {
-        ...props.style?.hover,
-      },
-    }),
-    '@container (max-width: 768px)': {
-      text: (props) => ({
-        ...props.style?.tablet,
-      }),
+    ...theme?.hover,
     },
-    '@container (max-width: 480px)': {
-      text: (props) => ({
-        ...props.style?.mobile,
-      }),
+  },
+  '@container (max-width: 768px)': {
+    text: {
+    ...theme?.tablet,
     },
-  }),
+  },
+  '@container (max-width: 480px)': {
+    text: {
+    ...theme?.mobile,
+    },
+  },
+}@End),
 );
 
 interface Props {
   text: string;
-  variant?: string;
-  uniqueid?: string;
 }
 
-type TextProps = React.HTMLAttributes<HTMLHeadingElement>;
+type TextProps = React.HTMLAttributes<HTMLParagraphElement>;
 
-export const CMSCaption = forwardRef<HTMLHeadingElement, TextProps & Props>(
-  ({ text, variant, uniqueid, ...rest }, ref) => {
-    const { values } = useCustomStyle();
-
-    const dataStyle = useMemo(() => {
-      return {
-        theme,
-        style: {
-          ...(variant ? values[variant] : values.CMSCaption),
-        },
-        id: uniqueid || uuidv4(),
-      };
-    }, [uniqueid, values, variant]);
-
-    const classes = useStyles(dataStyle);
+export const CMSCaption@variant = (
+  ({ text, ...rest }: TextProps & Props) => {
+    const classes = useStyles({ theme });
 
     return (
-      <p className={classes.text} key={uniqueid} ref={ref} {...rest}>
+      <p className={classes.text} {...rest}>
         {text}
       </p>
     );
--- src/libs/MainComponent/CMSLabel.tsx
+++ src/libs/MainComponent/CMSLabel.tsx
@@ -1,85 +1,65 @@
-import React, { forwardRef, useMemo } from 'react';
+import React from 'react';
 import { createUseStyles } from 'react-jss';
 import { theme, Theme } from '../theme';
-import { useCustomStyle } from '../../core/StyleContext';
-import { v4 as uuidv4 } from 'uuid';
 
-const useStyles = createUseStyles<string, { style: any }, any>(
-  (theme: Theme) => ({
-    text: (props) => ({
-      fontFamily: 'Montserrat, sans-serif',
-      color: theme.colors.black,
+const useStyles = createUseStyles<string, {}, any>(
+  (theme: Theme) => (@Start{
+  text: {
+    fontFamily: 'Montserrat, sans-serif',
+      color: '@Stheme.colors.black@E',
       fontWeight: 700,
       fontSize: 15,
       lineHeight: 1.5,
       margin: 0,
       width: 'fit-content',
       borderBottom:
-        props.style?.desktop?.borderBottom ||
-        props.style.desktop?.border ||
-        'none',
+    valueProps?.desktop?.borderBottom ||
+    valueProps.desktop?.border ||
+    'none',
       borderLeft:
-        props.style?.desktop?.borderLeft ||
-        props.style.desktop?.border ||
-        'none',
+    valueProps?.desktop?.borderLeft ||
+    valueProps.desktop?.border ||
+    'none',
       borderRight:
-        props.style?.desktop?.borderRight ||
-        props.style.desktop?.border ||
-        'none',
+    valueProps?.desktop?.borderRight ||
+    valueProps.desktop?.border ||
+    'none',
       borderTop:
-        props.style?.desktop?.borderTop ||
-        props.style.desktop?.border ||
-        'none',
-      ...props.style?.desktop,
+    valueProps?.desktop?.borderTop ||
+    valueProps.desktop?.border ||
+    'none',
+  ...valueProps?.desktop,
 
       '&:hover': {
-        ...props.style?.hover,
-      },
-    }),
-    '@container (max-width: 768px)': {
-      text: (props) => ({
-        ...props.style?.tablet,
-      }),
+    ...valueProps?.hover,
     },
-    '@container (max-width: 480px)': {
-      text: (props) => ({
-        ...props.style?.mobile,
-      }),
+  },
+  '@container (max-width: 768px)': {
+    text: {
+    ...valueProps?.tablet,
     },
-  }),
+  },
+  '@container (max-width: 480px)': {
+    text: {
+    ...valueProps?.mobile,
+    },
+  },
+}@End),
 );
 
 interface Props {
   text: string;
-  style?: React.CSSProperties;
-  variant?: string;
-  uniqueid?: string;
 }
 
-type TextProps = React.HTMLAttributes<HTMLHeadingElement>;
+type TextProps = React.HTMLAttributes<HTMLParagraphElement>;
 
-export const CMSLabel = forwardRef<HTMLHeadingElement, TextProps & Props>(
-  ({ text, style, variant, uniqueid, ...rest }, ref) => {
-    const { values } = useCustomStyle();
-
-    const dataStyle = useMemo(() => {
-      return {
-        theme,
-        style: {
-          ...(variant ? values[variant] : values.CMSLabel),
-        },
-        id: uniqueid || uuidv4(),
-      };
-    }, [uniqueid, values, variant]);
-
-    const classes = useStyles(dataStyle);
+export const CMSLabel@variant = (
+  ({ text, ...rest }: TextProps & Props) => {
+    const classes = useStyles({ theme });
 
     return (
       <p
         className={classes.text}
-        style={style}
-        key={uniqueid}
-        ref={ref}
         {...rest}
       >
         {text}
